a536cd0f6286f18d1c72cf51f38d73f8d2a25cff
Krayseer <krayseer@gmail.com>
2024-06-01 02:31:29 +0500

    test

--- ui-sources/src/app/components/header/header.component.ts
+++ ui-sources/src/app/components/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
-import {Router} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    RouterLink
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
--- ui-sources/src/app/layout/main-page/main-page.component.ts
+++ ui-sources/src/app/layout/main-page/main-page.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "../../components/header/header.component";
 import {FooterComponent} from "../../components/footer/footer.component";
+import {Router, RouterLink, RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
   imports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    RouterLink,
+    RouterOutlet
   ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
